edd488415b8da4af06a589db475450f5eec76456
David Hérault <dherault@gmail.com>
2022-01-20 04:05:38 +0200

    Update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,11 @@
 module.exports = {
   env: {
     browser: false,
-    es2021: true,
     node: true,
   },
   extends: [
     'dherault',
   ],
-  parser: 'babel-eslint',
   rules: {
     'import/no-extraneous-dependencies': 'off',
   },
